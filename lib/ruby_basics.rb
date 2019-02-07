2efe1e926b2da3c353cf4551d2e47de3eb2879f9
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-07 00:28:04 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ 42 / 7
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  greeting 
 end
 
 def return_a_value
-
+ return "Nice"
 end
 
 def last_evaluated_value
-
+  restaurant_name = "Guy's American Kitchen & Bar"
+  cuisine = "american"
+  motto = "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+ topping
 end
