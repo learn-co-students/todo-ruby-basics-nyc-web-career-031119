e29ddcb6d38b0f8f97718f1afdc5eab3cfcc7224
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-20 19:16:39 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
+  return name
 end
 
-def argue
-  
+def argue(x)
+  return x
 end
 
-def greeting
-
+def greeting(greeting, name)
+return
 end
 
 def return_a_value
-
+return "Nice"
 end
 
 def last_evaluated_value
-
+  status = "novice"
+  status = "professional"
+  status = "expert"
+return status
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+return topping
 end
