eb26877dd729b596f095dc2b5dd7bbe7da1a7afc
Theo Weller <weller.theodor@gmail.com>
2019-02-20 00:45:30 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,26 @@
 def division(num1, num2)
- 
+ return num1 / num2
 end
 
 def assign_variable(value)
-  
+  return value
 end
 
-def argue
-  
+def argue(argue)
+  return argue
 end
 
-def greeting
-
+def greeting(greeting, name)
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+"expert"
 end
 
-def pizza_party
-
+def pizza_party(value="cheese")
+  return value
 end
